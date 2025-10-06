b28eaba189611c9279e2ae52edfa60249564a5de
noaypunk <rnoaylovema@gmail.com>
2025-10-06 20:23:23 +0800

    user setter & getter update

--- eventify/src/main/java/com/teampura/eventify/UserEntity/User.java
+++ eventify/src/main/java/com/teampura/eventify/UserEntity/User.java
@@ -14,8 +14,25 @@ public class User {
     private String fname;
     private String lname;
     private String email;
-    private int mobileNum;
+    private long mobileNum;
     private String password;
     
+    public Long getId() { return userID; }
+    public void setId(Long id, long userID) { this.userID = userID; }
+
+    public String getFname() { return fname; }
+    public void setfname(String fname) { this.fname = fname; }
+
+    public String getLname() { return lname; }
+    public void setLname(String lname) { this.lname = lname; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public long getMobileNum() { return mobileNum; }
+    public void setMobileNum(long mobileNum) { this.mobileNum = mobileNum; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
 }
