bcf6e1a7e6345cb252b12abb045e5d65c4cf1913
noaypunk <rnoaylovema@gmail.com>
2025-10-06 20:30:37 +0800

    venue package update

--- eventify/src/main/java/com/teampura/eventify/FeedbackEntity/feedback.java
+++ eventify/src/main/java/com/teampura/eventify/FeedbackEntity/feedback.java
@@ -1,8 +1,14 @@
 package com.teampura.eventify.FeedbackEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    private long feedbackID;
 }
