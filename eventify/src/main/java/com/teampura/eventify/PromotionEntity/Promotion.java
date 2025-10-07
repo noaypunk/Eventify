9f0ff41d7adce5e97e1983c8a5f2725cb7c0ea11
Peter Macarulay <cit.pmm09@gmail.com>
2025-10-07 11:01:15 +0800

    Added event, promotion, and ticket entity attributes and accessors

--- eventify/src/main/java/com/teampura/eventify/PromotionEntity/Promotion.java
+++ eventify/src/main/java/com/teampura/eventify/PromotionEntity/Promotion.java
@@ -11,6 +11,28 @@ public class Promotion {
      @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private long promotionID;
-   
+    private long promoID;
+    private String promoName;
+    private String promoDesc;
+    private double promoDiscount;
+    private String startDate;
+    private String endDate;
+
+    public long getPromoID() { return promoID; }
+    public void setPromoID(long promoID) { this.promoID = promoID; }
+
+    public String getPromoName() { return promoName; }
+    public void setPromoName(String promoName) { this.promoName = promoName; }
+
+    public String getPromoDesc() { return promoDesc; }
+    public void setPromoDesc(String promoDesc) { this.promoDesc = promoDesc; }
+
+    public double getPromoDiscount() { return promoDiscount; }
+    public void setPromoDiscount(double discountRate) { this.promoDiscount = discountRate; }
+
+    public String getStartDate() { return startDate; }
+    public void setStartDate(String startDate) { this.startDate = startDate; }
+
+    public String getEndDate() { return endDate; }
+    public void setEndDate(String endDate) { this.endDate = endDate; }
 }
