df6b2d7a590ce52b7b8aa83dc6014c5eb809a34b
Denzel Don L. Sala <yorubun@gmail.com>
2025-10-22 14:06:26 +0800

    Updated User and Ticket Repositories

--- eventify/src/main/java/com/teampura/eventify/Repository/TicketRepository.java
+++ eventify/src/main/java/com/teampura/eventify/Repository/TicketRepository.java
@@ -8,5 +8,19 @@ import java.util.List;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    List<Ticket> findByUserId(Long userId);
+    
+    List<Ticket> findByEventName(String eventName);
+
+    List<Ticket> findByPrice(double price);
+
+    List<Ticket> findByStatus(String status);
+
+    List<Ticket> findByPriceGreaterThan(double price);
+
+    List<Ticket> findByPriceLessThan(double price);
+
+    List<Ticket> findByQuantityGreaterThan(int quantity);
+
+    List<Ticket> findByQuantityLessThan(int quantity);
+    
 }
--- eventify/src/main/java/com/teampura/eventify/Repository/UserRepository.java
+++ eventify/src/main/java/com/teampura/eventify/Repository/UserRepository.java
@@ -4,7 +4,20 @@ import com.teampura.eventify.Entity.UserEntity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    
+    Optional<User> findByEmail(String email);
+
+    List<User> findByFname(String fname);
+
+    List<User> findByLname(String lname);
+
+    Optional<User> findByMobileNum(Long mobileNum);
+
+    List<User> findByContainingIgnoreCase(String fname);
 }
